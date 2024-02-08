22cf4989d30672bd50e138404675e30adfb34508
Kyle Clapper <35474281+kclapper@users.noreply.github.com>
2024-02-08 13:11:44 -0500

    Update HomeController to use database query

--- Tulip/Controllers/HomeController.cs
+++ Tulip/Controllers/HomeController.cs
@@ -59,6 +59,12 @@ namespace Tulip.Controllers
             return View();
         }
 
+        private ApplicationUser getCurrentUser()
+        {
+            var userId = HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            return _db.ApplicationUsers.Find(userId);
+        }
+
         public async Task<ActionResult> Dashboard(string caseStudy = "MM")
         {
             try
@@ -72,26 +78,37 @@ namespace Tulip.Controllers
                 // }
                 // _db.SaveChanges();
 
-                var userInfo = new ApplicationUser();
+                //var userInfo = new ApplicationUser();
 
-                var userId = HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value;
-                _logger.LogInformation($"userId: {userId}");
+                //var userId = HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value;
+                var userInfo = getCurrentUser();
 
+                _logger.LogInformation($"Dashboard for user: {userInfo.UserName}\n" 
+                    + $"\tUserId: {userInfo.UserId}\n"
+                    + $"\tClientId: {userInfo.ClientId}\n"
+                    + $"\tApplicationServer: {userInfo.ApplicationServer}\n"
+                    + $"\tcaseStudy: {caseStudy}\n"
+                );
 
-                var userList = _db.ApplicationUsers.ToList().Where(q => q.UserId == HttpContext.User.Identity.Name.ToUpper());
-                foreach (var data in userList)
-                {
-                    userInfo = new ApplicationUser()
-                    {
-                        ApplicationServer = data.ApplicationServer,
-                        ClientId = data.ClientId,
-                        UserId = data.UserId
-                    };
-                }
+                // var userList = _db.ApplicationUsers.ToList().Where(q => q.UserId == HttpContext.User.Identity.Name.ToUpper());
+                // foreach (var data in userList)
+                // {
+                //     userInfo = new ApplicationUser()
+                //     {
+                //         ApplicationServer = data.ApplicationServer,
+                //         ClientId = data.ClientId,
+                //         UserId = data.UserId
+                //     };
+                // }
+
+                // TODO: Hardcoded credentials have to go.
                 var userName = "TEACH-003";
                 var passwd = "Naqiya99";
 
-                var url = GetUrl(userInfo.ClientId, userInfo.UserId, userInfo.ApplicationServer, caseStudy);
+                //var url = GetUrl(userInfo.ClientId, userInfo.UserId, userInfo.ApplicationServer, caseStudy);
+                var url = $"http://{userInfo.ApplicationServer.Trim()}/opu/odata/sap/ZUCC_GBM_GM_SRV/{caseStudy}_FSet(Id=2,User='{userInfo.UserId.ToUpper().Trim()}')?$format=json&sap-client={userInfo.ClientId}";
+
+                _logger.LogInformation($"SAP URL: {url}");
 
                 // Use this handler to allow untrusted SSL Certificates
                 var handler = new HttpClientHandler();
@@ -109,6 +126,8 @@ namespace Tulip.Controllers
                     Convert.ToBase64String(authToken));
 
                 var result = await client.GetAsync(url);
+
+                _logger.LogInformation($"SAP query result: {result}");
 
                 var content = await result.Content.ReadAsStringAsync();
 
